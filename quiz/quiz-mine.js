8ba33ad0aa5a39385dca2197f96c974a285f40ba
hkhali24 <hkhalique@outlook.com>
2025-03-12 17:08:20 -0400

    updates

---
+++ quiz/quiz-mine.js
@@ -0,0 +1,34 @@
+const quizData = {
+  "title": "My Portfolio Quiz",
+  "questions": [
+    {
+        "question": "What was my major?",
+        "options": ["Psychology", "Arts Management", "Marketing", "Information Technology"],
+        "answer": "Marketing",
+        "type": "single-answer"
+      },
+       {
+        "question": "What are the three Primary colors?",
+        "options": ["Red, Yellow, and Blue", "Yellow, Blue, and Purple", "Red, Purple, and Orange", "Green, Yellow, and Orange"],
+        "answer": "Red, Yellow, and Blue",
+        "type": "single-answer"
+      },
+      {
+        "question": "What color does the background change to when it is on Dark Mode?",
+        "options": ["Dark Purple", "Dark Green", "Black", "Navy Blue"],
+        "answer": "Navy Blue",
+        "type": "single-answer"
+      },
+      {
+        "question": "Which of the following are font styles.",
+        "options": ["Decorative", "StylishFont", "SansSerif", "Monospaced"],
+        "answers": ["Decorative", "SansSerif", "Monospaced"],
+        "type": "multiple-answer"
+      },
+      {
+        "question": "What is the name of the model on the Digital Magazine Cover project?",
+        "answers": ["Maribella"],
+        "type": "free-form"
+    }
+  ]
+}
